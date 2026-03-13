254f6c2ec35719c3571a9a81555bf537c68185e0
Mikołaj Hoffmann <e.m4il@op.pl>
2026-03-13 14:16:18 +0100

    updated backend to get better and more specific response

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import io
 from ultralytics import YOLO
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 model = YOLO("yolov8n.pt")
 
@@ -14,9 +24,18 @@ async def predict(file: UploadFile = File(...)):
     
     results = model(image)
     
-    detected = []
+    detections = []
+
     for r in results:
-        for c in r.boxes.cls:
-            detected.append(model.names[int(c)])
+        for box in r.boxes:
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            conf = float(box.conf[0])
+            cls = int(box.cls[0])
+            
+            detections.append({
+                "name": model.names[cls],
+                "confidence": round(conf * 100, 2),
+                "box": [x1, y1, x2, y2]
+            })
     
-    return {"objects": list(set(detected))}
+    return {"detections": detections}
